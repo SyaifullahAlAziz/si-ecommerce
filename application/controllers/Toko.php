e882e336e74149c27f18feb005af0897d8361dcd
LordRiyan <alamat.emailnya.riyan@gmail.com>
2022-07-05 13:55:51 +0700

    Update

--- application/controllers/Toko.php
+++ application/controllers/Toko.php
@@ -6,9 +6,13 @@ class Toko extends CI_Controller
 
 	public function index()
 	{
+		// Load Model
+		$this->load->model('tokoModel');
+
 		// Load view
 		$this->load->view('tokos', [
 			'parent' => 'toko',
+			'semua' => $this->tokoModel->all()
 		]);
 	}
 }
---
+++ application/models/tokoModel.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class TokoModel extends CI_Model
+{
+
+	public function all()
+	{
+		return [1,2,3,4];
+		
+	}
+	
+}
